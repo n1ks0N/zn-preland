ad319d20973467d45ab91f40521ebecdcfde56d4
niksON <gromyakov05@mail.ru>
2020-12-11 11:06:46 +0300

    master

---
+++ land.js
@@ -0,0 +1,11 @@
+var land = {
+    bg: '#fff',
+    favicon: 'https://ssl.gstatic.com/atari/images/favicon_2.ico',
+    name: 'A-cardin ®',
+    title: 'SKĄD JESTEŚ?',
+    btnFirstText: 'Polska',
+    btnFirstLink: '#',
+    btnSecText: 'Inne państwo',
+    btnSecLink: '#',
+    img: ''
+};
